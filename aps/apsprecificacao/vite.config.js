bdd23b05ed55d6d68c674e263cedecbdb3c6abcc
afabricamkte <filipe.paula.f1@gmail.com>
2025-09-20 12:58:50 +0000

    Precificação 3

--- aps/apsprecificacao/vite.config.js
+++ aps/apsprecificacao/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/aps/apsprecificacao/',
+  base: './',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
